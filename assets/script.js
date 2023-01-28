bc4bccf0d6e86f396ecc32b3c72e5ec3409222a7
Will Oliver <studioswilloliver@gmail.com>
2023-01-28 18:23:47 -0300

    finalizado o css e iniciado o js do projeto

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const cardName = document.querySelector('#name')
+const cardNumber = document.querySelector('#number')
+const cardMounth = document.querySelector('#mounth')
+const cardYear = document.querySelector('#year')
+const cardCvc = document.querySelector('#cvc')
+
+
