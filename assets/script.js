d88f32a891bcc39ad9c37cca89fe3dc496abb232
Will Oliver <studioswilloliver@gmail.com>
2023-01-29 14:58:28 -0300

    continuação da escrita do código js

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ const btnConfirm = document.querySelector('#btn-confirm');
 const cardTxtNumbers = document.querySelector('#card-txt-numbers')
 const cardTxtName = document.querySelector('#card-name')
 const cardTxtDate = document.querySelector('#card-date')
+const cardTxtCvc = document.querySelector('#information-cvc')
 
 const cardName = document.querySelector('#name')
 const cardNumber = document.querySelector('#number')
@@ -12,31 +13,40 @@ const cardMounth = document.querySelector('#mounth')
 const cardYear = document.querySelector('#year')
 const cardCvc = document.querySelector('#cvc')
 
+const erro = document.querySelector('.erro')
 
-/*form.addEventListener('submit', (event) => {
+
+form.addEventListener('submit', (event) => {
     event.preventDefault()
     const dados = {
-        name: cardName,
-        number: cardNumber,
-        mounth: cardMounth,
-        year: cardYear,
-        cvc: cardCvc
+        name: cardName.value,
+        number: cardNumber.value,
+        mounth: cardMounth.value,
+        year: cardYear.value,
+        cvc: cardCvc.value
     }
 
     validarInput()
-})*/
+    console.log(dados)
+})
 
 cardNumber.addEventListener('input', (event) => {
     let numero = event.target.value + '0'.repeat(16 - event.target.value.length)
     cardTxtNumbers.innerHTML = numero.slice(0,4)+" "+numero.slice(4,8)+" "+numero.slice(8,12)+" "+numero.slice(12)
 
     if (cardNumber.value != Number(cardNumber.value)) {
-        const erro = document.createElement('span')
+        erro.innerHTML = 'Digite apenas números!'
+    } else{
+        erro.innerHTML = ''
     }
 })
 
-cardName.addEventListener('input', () => {
+cardName.addEventListener('input', (event) => {
+    cardTxtName.innerHTML = event.target.value ? event.target.value : 'e.g. Jane Appleseed'
+})
 
+cardCvc.addEventListener('input', (event) => {
+    cardTxtCvc.innerHTML = event.target.value + '0'.repeat(3 - event.target.value.length) ? event.target.value + '0'.repeat(3 - event.target.value.length) : '000'
 })
 
 
