30d14fe4bcb193d49a90cffcb6d24add2c7e7d0f
Will Oliver <studioswilloliver@gmail.com>
2023-01-30 16:17:57 -0300

    notificações de erro adicionadas aos input

--- assets/script.js
+++ assets/script.js
@@ -9,20 +9,22 @@ const cardTxtCvc = document.querySelector('#information-cvc')
 
 const cardName = document.querySelector('#name')
 const cardNumber = document.querySelector('#number')
-const cardMounth = document.querySelector('#mounth')
-const cardYear = document.querySelector('#year')
+const cardDate = document.querySelectorAll('.date')
 const cardCvc = document.querySelector('#cvc')
 
-const erro = document.querySelector('.erro')
+/*************ERROS*************/
+
+const erroName = document.querySelector('.erro-Name')
+const erroNumber = document.querySelector('.erro-Number')
+const erroDate = document.querySelector('.erro-Date')
+const erroCvc = document.querySelector('.erro-Cvc')
 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    const dados = {
+    let dados = {
         name: cardName.value,
         number: cardNumber.value,
-        mounth: cardMounth.value,
-        year: cardYear.value,
         cvc: cardCvc.value
     }
 
@@ -30,29 +32,41 @@ form.addEventListener('submit', (event) => {
     console.log(dados)
 })
 
-cardNumber.addEventListener('input', (event) => {
-    let numero = event.target.value + '0'.repeat(16 - event.target.value.length)
-    cardTxtNumbers.innerHTML = numero.slice(0,4)+" "+numero.slice(4,8)+" "+numero.slice(8,12)+" "+numero.slice(12)
 
-    if (cardNumber.value != Number(cardNumber.value)) {
-        erro.innerHTML = 'Digite apenas números!'
-    } else{
-        erro.innerHTML = ''
-    }
-})
 
 cardName.addEventListener('input', (event) => {
     cardTxtName.innerHTML = event.target.value ? event.target.value : 'e.g. Jane Appleseed'
 })
 
+cardNumber.addEventListener('input', (event) => {
+    let numero = event.target.value + '0'.repeat(16 - event.target.value.length)
+    cardTxtNumbers.innerHTML = numero.slice(0,4)+" "+numero.slice(4,8)+" "+numero.slice(8,12)+" "+numero.slice(12)
+
+    if (cardNumber.value != Number(cardNumber.value)) {
+        erroNumber.innerHTML = 'Wrong format, numbers only'
+    } else{
+        erroNumber.innerHTML = ''
+    }
+})
+
+
 cardCvc.addEventListener('input', (event) => {
     cardTxtCvc.innerHTML = event.target.value + '0'.repeat(3 - event.target.value.length) ? event.target.value + '0'.repeat(3 - event.target.value.length) : '000'
+
+    if (cardCvc.value != Number(cardCvc.value)) {
+        erroCvc.innerHTML = 'Wrong format, numbers only'
+    } else{
+        erroCvc.innerHTML = ''
+    }
+
+    if (!cardCvc.value) {
+        erroCvc.innerHTML = "Can't be blank"
+    }
 })
 
 
 function validarInput() {
-    if (!cardName.value || !cardNumber.value || !cardMounth || !cardCvc.value || !cardYear.value) {
-        console.log('Preencha os dados!')
+    if (!cardName.value || !cardNumber.value || !cardDate.value || !cardCvc.value) {
     }
 }
 
